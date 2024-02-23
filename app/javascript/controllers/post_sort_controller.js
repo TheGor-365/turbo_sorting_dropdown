e9b82ef66d712914e6d21aa8441891aad4724c80
Gor <7495500@gmail.com>
2024-02-23 16:45:53 +0300

    Add seeds

---
+++ app/javascript/controllers/post_sort_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="post-sort"
+export default class extends Controller {
+  connect() {
+  }
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+5.times do |index|
+  post = Post.create(
+    title:      "Title #{index.next}",
+    content:    "Content #{index.next}",
+    created_at: (Time.now - index.days)
+  )
+  puts post.title
+end
